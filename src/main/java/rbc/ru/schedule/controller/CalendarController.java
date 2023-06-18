81b876a6cad55ef18f199b26f2fe13c313b6f392
Petr Abramov <petya.abramov.2015@bk.ru>
2023-06-18 12:04:07 +0300

    filterParamDTO added

--- src/main/java/rbc/ru/schedule/controller/CalendarController.java
+++ src/main/java/rbc/ru/schedule/controller/CalendarController.java
@@ -4,13 +4,16 @@ import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import rbc.ru.schedule.entity.GroupEntity;
 import rbc.ru.schedule.entity.ProjectEntity;
+import rbc.ru.schedule.entity.ToDoEntity;
+import rbc.ru.schedule.service.GroupServiceImpl;
 import rbc.ru.schedule.service.ProjectServiceImpl;
 import rbc.ru.schedule.validator.UserValidator;
 
+import javax.validation.Valid;
 import java.security.Principal;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -26,9 +29,12 @@ public class CalendarController {
     @Autowired
     ProjectServiceImpl projectService;
 
+    @Autowired
+    GroupServiceImpl groupService;
+
     @GetMapping("minusMonth")
     public String minusMonth(Model model,
-                            @RequestParam String dateString) {
+                             @RequestParam String dateString) {
         LocalDate date = LocalDate.parse(dateString);
         date = date.minusMonths(1);
 
@@ -44,10 +50,13 @@ public class CalendarController {
         return "redirect:/schedule/month?dateString=" + date.toString();
     }
 
-    @GetMapping("month{dateString}")
+    @GetMapping("month{dateString}{oneGroup}{group_id}")
     public String month(Model model,
                         Principal principal,
-                        @RequestParam(defaultValue = "") String dateString) {
+                        @RequestParam(name = "oneGroup", defaultValue = "false") String justOneGroup,
+                        @RequestParam(name = "group_id", defaultValue = "1") String group_id,
+                        @RequestParam(name = "dateString", defaultValue = "") String dateString
+    ) {
 
         LocalDate date;
         try {
@@ -55,14 +64,15 @@ public class CalendarController {
         } catch (DateTimeParseException e) {
             date = LocalDate.now();
         }
+        Boolean oneGroup = Boolean.parseBoolean(justOneGroup); //TODO check an exception
 
         LocalDate[][] list = new LocalDate[5][7];
         LocalDate d = date.minusDays(date.getDayOfMonth() - 1); // first day of the current month
         d = d.minusDays(d.getDayOfWeek().getValue() - 1); // found first Monday before the first day of the month
 
         LocalDateTime start = d.atStartOfDay();
-        LocalDateTime stop = d.plusDays(7*5).atStartOfDay();
-        CellDTO [][] cells = new CellDTO[5][7];
+        LocalDateTime stop = d.plusDays(7 * 5).atStartOfDay();
+        CellDTO[][] cells = new CellDTO[5][7];
 
         Set<ProjectEntity> allProjectsInPeriod = projectService.findInPeriod(start, stop);
 
@@ -77,13 +87,35 @@ public class CalendarController {
         model.addAttribute("userName", principal.getName());
         model.addAttribute("isAdmin", userValidator.isAdmin(principal.getName()));
 
-        model.addAttribute("date", date);
         model.addAttribute("cells", cells);
+
+        FilterParamDTO filterParam = new FilterParamDTO();
+        filterParam.setOneGroup(oneGroup);
+        if (oneGroup)
+            filterParam.setGroup(groupService.getById(Long.parseLong(group_id)));
+        else
+            filterParam.setGroup(groupService.getById(1L));
+
+        filterParam.setDate(date);
+
+        System.out.println("list : "+filterParam);
+
+        model.addAttribute("groups", groupService.getByName(""));
+        model.addAttribute("filterParam", filterParam);
 
         return "month";
     }
-}
 
+    @PostMapping("monthParam")
+    public String param(Model model,
+                        @ModelAttribute("filterParam") FilterParamDTO filterParam) {
+
+        if (filterParam.getOneGroup())
+            return "redirect:/schedule/month?dateString=" + filterParam.getDate().toString() + "&oneGroup=true" + "&group_id=" + filterParam.getGroup().getId();
+
+        return "redirect:/schedule/month?dateString=" + filterParam.getDate().toString() + "&oneGroup=false";
+    }
+}
 @Data
 class CellDTO {
     LocalDate date;
@@ -92,3 +124,20 @@ class CellDTO {
         this.list= new HashSet<>();
     }
 }
+
+@Data
+class FilterParamDTO {
+    LocalDate date;
+    Boolean oneGroup;
+    GroupEntity group;
+
+    @Override
+    public String toString() {
+        return "FilterParamDTO{" +
+                "date=" + date +
+                ", oneGroup=" + oneGroup +
+                ", group=" + group.getName() +
+                '}';
+    }
+}
+
